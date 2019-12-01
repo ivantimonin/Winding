aa34f4a5353736c9e3b588f194bdcd838139f267
timon <timon@DESKTOP-ANCORQQ>
2019-12-01 04:12:10 +0300

    Добавьте файлы проекта.

---
+++ Model/Wire_type.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winding
+{
+    class Wire_type
+    {
+        public string Name { get; private set;}// тип выбранного провода
+        public double a_size { get; private set; }//меньшая сторона элементарного провода
+        public double b_size { get; private set; }//большая сторона элементарного провода
+        public double isolation_size { get; private set; }//размер изоляции на две стороны
+        public double number_turns { get; private set; } = 3;//числов витков
+        public Wire_type()
+        {           
+                   
+        }
+        public string Paint(double a, double b, double number_turns, double Z,
+                            int Field_numeric, double left_border, double right_border)
+        {
+            double pole_size=(right_border - left_border)/26;
+            left_border += pole_size;
+            right_border -= pole_size;
+            double height_turn = Z+b;// высота витка 
+            double x1 = pole_size*Field_numeric+ pole_size/2;//будет задавать пользователь( поле захода обмотки)
+            double y1 = 150;//координата начала отрисовки по y
+            double height_inclien = height_turn*(right_border-x1)/(right_border- left_border);//высота наклона
+            string up_winding_coord="";
+            for (int turn = 0; turn < Math.Floor(number_turns); turn++)//отрисовка целых витков
+            {                
+                double x2 = right_border;
+                double y2 = y1 + height_inclien;
+                double x3 = right_border;
+                double y3 = y2 + height_turn;
+                double x4 = left_border;
+                double y4 = y2;
+                double x5 = left_border;
+                double y5 = y4 + height_turn;
+                double x6 = x1;
+                double y6 = 2*height_turn+y1;
+                double[] coord = { x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6};
+                up_winding_coord = Add_coord(coord, up_winding_coord, turn);
+                y1 = y1+ height_turn;
+                //отрисовка дробных витков
+                if (number_turns - Math.Floor(number_turns) != 0 && turn == Math.Floor(number_turns) - 1)
+                {
+                    double not_int_turn = number_turns - Math.Floor(number_turns); //часть дроб-го витка
+                    if (x6 + (x2 - x4) * not_int_turn > x2)//переход на след-щий уровень
+                    {
+                        
+                        double[] part_coord = {//хорошо бы убрать
+                            x3,y3+height_turn,
+                            x3,y3,
+                            x6,y6-height_turn,
+                            x6,y6,
+                            x5,y5,
+                            x5,y5+height_turn,
+                            x5 +((x2 - x5) * not_int_turn-(x3-x6)),y5+height_turn+height_inclien*
+                            ((x2 - x5) * not_int_turn - (x3 - x6))/(x2-x1)};
+                            
+                        up_winding_coord = Add_coord(part_coord, up_winding_coord, turn);
+                    }        
+                }                    
+            }           
+            
+
+            return up_winding_coord;
+        }
+        private string Add_coord(double[]x_or_y, string up_winding_coord, int turn)
+        {
+            for (int i=0;i<x_or_y.Length;i++ )
+            {                
+                string _x_or_y = Convert.ToString(x_or_y[i]).Replace(",", ".");
+                if (i == 0 && turn==0)
+                {
+                    up_winding_coord = _x_or_y;
+                }                
+                else
+                {
+                    up_winding_coord += "," + _x_or_y;
+                }
+            }     
+            return up_winding_coord;
+        }
+    }
+}
---
+++ View_model/MainVM.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+
+namespace Winding
+{
+    public class MainVM : INotifyPropertyChanged
+    {  
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private double _a;//меньшая сторона провода        
+        public double a
+        {
+            get { return _a; }
+            set
+            {
+                _a = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+
+        private double _b;//большая сторона провода
+        public double b
+        {
+            get { return _b; }
+            set
+            {
+                _b = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+        private double _Z;//изоляция провода
+        public double Z
+        {
+            get { return _Z; }
+            set
+            {
+                _Z = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+
+        private double _number_turns;//число витков        
+        public double number_turns
+        {
+            get { return _number_turns; }
+            set
+            {
+                _number_turns = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+        private string _type_of_wire;//тип провода     
+        public string type_of_wire
+        {
+            get { return _type_of_wire; }
+            set
+            {
+                _type_of_wire = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+        
+
+        private string _right_bord="500";
+        public string right_bord
+        {
+            get { return _right_bord; }
+            set
+            {     
+                _right_bord = value;                
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }
+
+
+
+        private int _N;//кол-во элементарок     
+        public int N
+        {
+            get { return _N; }
+            set
+            {
+                MessageBox.Show($"Переменная=");
+                _N = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+
+            }
+        }
+
+        private int _Field_numeric = 12;//поле захода витка    
+        public int Field_numeric
+        {
+            get { return _Field_numeric; }
+            set
+            {
+                if (value > 24)
+                {
+                    value = 24;
+                }
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                _Field_numeric = value;
+                OnPropertyChanged("coord"); // уведомление View о том, что изменились координаты
+            }
+        }        
+       
+       
+        private double left_border = 0;
+
+      
+
+        Wire_type some_wire = new Wire_type();
+        private string _coord;
+        public string coord
+        {
+            get {                
+                return _coord= some_wire.Paint(a,b, number_turns,Z, Field_numeric,left_border, Convert.ToDouble(right_bord)); }
+            set {
+
+            }
+        }
+        
+        
+
+
+
+
+
+
+    }
+
+
+}
